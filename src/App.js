c536c7db26caa6cc4b5da5b923d185d3b44f53ca
amitdtu <amitdtu007@gamail.com>
2020-09-28 11:13:28 +0530

    add demo image

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import "./assets/hero.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./Container/HomePage";
 import Login from "./Container/Login";
 import Header from "./Component/Header";
@@ -9,13 +9,10 @@ import Header from "./Component/Header";
 class App extends Component {
   render() {
     return (
-      <Router>
+      <>
         <Header />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </Router>
+        <HomePage />
+      </>
     );
   }
 }
